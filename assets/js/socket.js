8177b4e431f34f4f18cd86818bc634d96c0ea770
Julien Biezemans <jb@jbpros.com>
2017-09-27 10:42:22 +0200

    Remove comments

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -1,8 +1,3 @@
-// NOTE: The contents of this file will only be executed if
-// you uncomment its entry in "assets/js/app.js".
-
-// To use Phoenix channels, the first step is to import Socket
-// and connect at the socket path in "lib/web/endpoint.ex":
 import { Socket } from "phoenix"
 import Vue from 'vue/dist/vue.common'
 
@@ -54,9 +49,7 @@ const socket = new Socket("/socket", {params: {token: window.userToken, nickname
 
 socket.connect()
 
-// Now that you are connected, you can join channels with a topic:
-const channel           = socket.channel("room:lobby", {})
-const messagesContainer = document.querySelector("#messages")
+const channel = socket.channel("room:lobby", {})
 
 new Vue({
   el: "#chat-input",
