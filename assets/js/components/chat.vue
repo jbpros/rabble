42a34cb7ad25a777bad1c45def2f6cab607c7c19
Julien Biezemans <jb@jbpros.com>
2017-10-04 14:53:53 +0100

    Extract Login

--- assets/js/components/chat.vue
+++ assets/js/components/chat.vue
@@ -7,22 +7,21 @@
     </div>
     <div v-else>
       Not connected.
-      <form v-on:submit.prevent="connect">
-        <input v-model="nickname" placeholder="Your nickname..."></input>
-        <button type="submit">Connect</button>
-      </form>
+      <login></login>
     </div>
   </div>
 </template>
 
 <script>
 import ChatInput from './chat-input.vue'
+import Login from './login.vue'
 import Messages from './messages.vue'
 import Participants from './participants.vue'
 
 export default {
   components: {
     ChatInput,
+    Login,
     Messages,
     Participants,
   },
@@ -32,20 +31,6 @@ export default {
     },
     isConnecting() {
       return this.$store.getters.isConnecting
-    },
-  },
-  data() {
-    return {
-      nickname:
-        (window.location.search && window.location.search.slice(1)) || 'nobody',
-    }
-  },
-  methods: {
-    connect: function() {
-      this.$store.dispatch('connect', {
-        token: window.userToken,
-        nickname: this.nickname,
-      })
     },
   },
 }
---
+++ assets/js/components/login.vue
@@ -0,0 +1,25 @@
+<template>
+  <form v-on:submit.prevent="connect">
+    <input v-model="nickname" placeholder="Your nickname..."></input>
+    <button type="submit">Connect</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      nickname:
+        (window.location.search && window.location.search.slice(1)) || 'nobody',
+    }
+  },
+  methods: {
+    connect: function() {
+      this.$store.dispatch('connect', {
+        token: window.userToken,
+        nickname: this.nickname,
+      })
+    },
+  },
+}
+</script>
