dca057be519b5d351a594f5fdbdb46b9b927df9f
Julien Biezemans <jb@jbpros.com>
2017-10-26 10:19:39 +0200

    Make timer more useful

--- assets/js/components/room/timer.vue
+++ assets/js/components/room/timer.vue
@@ -1,24 +1,42 @@
 <template>
   <div>
-    <span>{{ remainingSeconds }}</span>
-    <form v-on:submit.prevent="start">
-      <button type="submit">Start</button>
-    </form>
+    <p>{{ remainingSeconds | asTime }}</p>
+    <button v-on:click="start(300)">5m</button>
+    <button v-on:click="start(600)">10m</button>
+    <button v-on:click="start(900)">15m</button>
+    <button v-on:click="start(1200)">20m</button>
+    <button v-on:click="adjust(-30)">-30s</button>
+    <button v-on:click="adjust(30)">+30s</button>
   </div>
 </template>
 
 <script>
+const pad = n => String(n).padStart(2, '0')
+
 export default {
   data() {
-    return { remainingSeconds: 1 }
+    return { remainingSeconds: 0 }
   },
   components: {},
   created() {
     this.update()
   },
+  filters: {
+    asTime(seconds) {
+      const mins = Math.floor(seconds / 60)
+      const secs = Math.floor(seconds) % 60
+      return `${pad(mins)}:${pad(secs)}`
+    },
+  },
   methods: {
-    start() {
-      this.$store.dispatch('startTimer', { durationSeconds: 30 })
+    start(seconds) {
+      this.$store.dispatch('startTimer', { durationSeconds: seconds })
+    },
+
+    adjust(seconds) {
+      this.$store.dispatch('startTimer', {
+        durationSeconds: Math.floor(this.remainingSeconds + seconds),
+      })
     },
 
     update() {
