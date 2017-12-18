e458878fc8ec998c6e2bab8b52cef1f820b6449f
Julien Biezemans <jb@jbpros.com>
2017-12-18 15:44:50 +0100

    Bump to 0.0.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Rabble.Mixfile do
   def project do
     [
       app: :rabble,
-      version: "0.0.2",
+      version: "0.0.3",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
